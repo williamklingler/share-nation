2b4afca75eda46d6e525479c0638b7ff584d3649
William Klingler <klinglew@gmail.com>
2020-05-07 16:26:31 -0400

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage.js'
+import HomePage from './components/HomePage.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-        <LoginPage/>
-  );
+
+class App extends React.Component {
+  render(){
+    return (
+      <Router>
+      <div>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+         <Route path="/">
+           <HomePage/>
+         </Route>
+       </Switch>
+     </div>
+   </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 class LoginPage extends React.Component {
   render() {
-    return <h1>Hello, my name is Alex and I am a big dick nigga!!!</h1>;
+    return (
+      <h1> Login page </h1>
+    )
   }
 }
 
