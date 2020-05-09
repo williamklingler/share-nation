ad0f43aff2a6fd00c7f9145261ca4f3576925f73
William Klingler <klinglew@gmail.com>
2020-05-09 17:35:12 -0400

    added google cloud functions API functionality

--- src/components/SignedIn.js
+++ src/components/SignedIn.js
@@ -5,16 +5,23 @@ import authContext from '../authentication-context.js'
 class SignedIn extends React.Component{
   constructor(props){
     super(props);
-    this.state = {signedIn: this.props.signedIn};
+    this.state = {apiResponse: null};
   }
-  componentWillReceiveProps(nextProps){
-    this.setState({signedIn: nextProps.signedIn});
-  }
+  callAPI() {
+    fetch("https://us-central1-simp-nation.cloudfunctions.net/simpAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
   render(){
     return(
       <div>
         <h1> You are {!this.context.user && <div>not</div>} signed in</h1>
         {this.context.user && <p> Welcome {this.context.user.displayName}! </p> }
+        {this.state.apiResponse && <h2> This is the api response: {this.state.apiResponse} </h2>}
         <p> This is all the info we have about you right now:
         <br/> <br/> {JSON.stringify(this.context.user)}
          </p>
