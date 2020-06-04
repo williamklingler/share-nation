6236f1ab2fc7c5ce4a86331ee8ee0cd749a6fbed
William Klingler <klinglew@gmail.com>
2020-06-04 12:49:03 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,7 @@
 import * as firebase from "firebase/app";
 
 const config = {
-    apiKey: "AIzaSyCom7f-oJOQAzjlT59Q7BvK2CvRRV3Bm78",
-    authDomain: "simp-nation.firebaseapp.com",
-    databaseURL: "https://simp-nation.firebaseio.com",
-    projectId: "simp-nation",
-    storageBucket: "simp-nation.appspot.com",
-    messagingSenderId: "12607800366",
-    appId: "1:12607800366:web:f16ab615cc342c33330bbf",
-    measurementId: "G-HR1HFC7ZEY"
+    //redacted, will cause errors for now
   };
 
   firebase.initializeApp(config);
